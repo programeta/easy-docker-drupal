be5401ea8c39d5d68db992b8bc9ed7079a7b4426
Oscar Embún <oscar.embun.utrillas@everis.com>
2020-10-31 07:55:08 +0100

    Perform initialize script

--- initialize.sh
+++ initialize.sh
@@ -7,29 +7,39 @@ YELLOW='\033[1;33m'
 PURPLE='\033[0;35m'
 
 printf '\n'
-printf "Please, input the name of the project (as machinename)?:"
+printf "Please, input the name of the project (as machinename): "
 read PROJECT_NAME
 
 printf "${GREEN}Configuring environment...${NC}\n"
-echo "\nPROJECT_NAME=${PROJECT_NAME}" >> .env
+printf "  Modifying environment name...${NC}\n"
+sed -i "s/PROJECT_NAME=vm/PROJECT_NAME=${PROJECT_NAME}/g" .env
 
 #Generate certificates for environment
 FILE=~/.ssh/id_rsa
 if [ ! -f "$FILE" ]; then
+    printf "  Generating SSH keys...${NC}\n"
     ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
 fi
 
 #Copy certificates from local into environment
+printf "  Copying SSH keys...${NC}\n"
 cp ~/.ssh/id_rsa conf/php/ssh/id_rsa
 cp ~/.ssh/id_rsa.pub conf/php/ssh/id_rsa.pub
 
 #Generate certificate for HTTPS
+printf "  Generating SSL certificate for apache...${NC}\n"
 openssl req -x509 -new -newkey rsa:4096 -nodes -days 2048 \
     -keyout conf/apache/ssl/localhost.key -out conf/apache/ssl/localhost.crt \
     -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=local.vm" > /dev/null 2>&1
 
-printf "\n${GREEN}Environment configured...${NC}\n"
-printf "\nNow you can modify the file ${BLUE}'conf/php/virtualhost.conf'${NC} to add any projects as you need."
-printf "\nRemember that folder ${BLUE}'html'${NC} is shared in php container in path ${BLUE}'/var/www/html'${NC} to configure properly DocumentRoot in your virtualhost"
-printf "\n"
+#Configure VirtualHost
+printf "  Modifying Virtualhost...${NC}\n"
+sed -i "s/your.domain/${PROJECT_NAME}.vm/g" conf/php/virtualhost.conf
+sed -i "s/your.docroot.project/${PROJECT_NAME}/g" conf/php/virtualhost.conf
 
+
+printf "${GREEN}Environment configured...${NC}\n"
+printf "  Virtualhost configuration...\n"
+printf "   * Domain configured as ${PURPLE}https://${PROJECT_NAME}.vm${NC}\n"
+printf "   * Document root configured in ${PURPLE}/var/www/html/${PROJECT_NAME}/web${NC}\n"
+printf "\n"
